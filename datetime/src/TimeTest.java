9e4a015b49f7ce3cfe435c4759497312aebed269
Todd O'Bryan <toddobryan@gmail.com>
2013-09-23 08:00:22 -0400

    added a TimeTest to datetime

---
+++ datetime/src/TimeTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeTest {
+	Time t1 = new Time(0, 0, 0);
+	Time t2 = new Time(6, 1, 2);
+	Time t3 = new Time(11, 59, 59);
+	Time t4 = new Time(12, 0, 0);
+	Time t5 = new Time(15, 15, 15);
+	Time t6 = new Time(20, 2, 30);
+	Time t7 = new Time(21, 30, 5);
+	Time t8 = new Time(23, 59, 59);
+	
+}	
